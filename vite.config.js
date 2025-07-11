cdcafbb6c8e5be6cabd54cadf75e05de1f6328f3
theNkennaAmadi <nkenna@theblackpepperstudio.com>
2025-07-11 21:23:09 +0100

    update allowedHosts in vite.config.js to include starter-vite-template.pages.dev

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig(
         },
         server: {
             cors: true,
-            allowedHosts: ['.webflow.io', '.pages.dev', '.localcan.dev'] // Adjust the allowed hosts as needed
+            allowedHosts: ['.webflow.io', 'starter-vite-template.pages.dev/', '.localcan.dev'] // Adjust the allowed hosts as needed
 
         }
     }
